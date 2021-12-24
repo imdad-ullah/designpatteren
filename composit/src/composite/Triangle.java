c523c8becc7f790f0906af7ca2990e7df0178813
imdad-ullah <76532811+imdad-ullah@users.noreply.github.com>
2021-12-24 08:06:33 -0600

    Add files via upload

---
+++ composit/src/composite/Triangle.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composite;
+
+/**
+ *
+ * @author imdad
+ */
+import java.awt.Color;
+import java.awt.*;
+public class Triangle extends BaseShape {
+    public int radius;
+    int[] x;
+    int[] y;
+
+    public Triangle(int[] x, int[] y, Color color) {
+        super(x[0], y[0], color);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    @Override
+    public int getWidth() {
+        return radius * 2;
+    }
+
+    @Override
+    public int getHeight() {
+        return radius * 2;
+    }
+
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        graphics.drawPolygon(x, y, 3);
+        
+       
+    }
+}
