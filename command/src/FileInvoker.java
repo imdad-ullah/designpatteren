73bd453124442a7b8b6be9b500e2a958da3c1708
imdad-ullah <76532811+imdad-ullah@users.noreply.github.com>
2021-12-24 08:07:58 -0600

    Add files via upload

---
+++ command/src/FileInvoker.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+public class FileInvoker {
+	// implement stack
+	Command cmd;
+        boolean isExecuted;
+	public FileInvoker(Command cmd) {
+		this.cmd = cmd;
+	}
+	void execute(){
+		cmd.execute();
+                isExecuted=true;
+	}
+	void undo(){
+		cmd.undo();
+                isExecuted=false;
+	}
+	
+}
